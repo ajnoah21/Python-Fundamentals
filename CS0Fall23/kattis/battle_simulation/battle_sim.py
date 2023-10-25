40ed753cab8bc718e8171e636867628aca164881
Corin Chepko <cchepko2@coloradomesa.edu>
2023-10-25 11:51:26 -0600

    asf

---
+++ CS0Fall23/kattis/battle_simulation/battle_sim.py
@@ -0,0 +1,36 @@
+'''
+Name: Corin Chepko
+Date: 10/25/23
+Program Description: https://open.kattis.com/problems/battlesimulation
+Algorithm:
+    input a string
+    for each char in string, output counter char, 'R'>'S','B'>'K','L'>'H'
+    if see LBR,LRB,BRL,BLR,RBL,RLB ect..., output 'C' to counter
+'''
+def test():
+    pass
+
+def main():
+    moves = input()
+    combos = ('LBR','LRB','BRL','BLR','RBL','RLB')
+    for combo in combos:
+        moves=moves.replace(combo,'C') #Replace all combos with 'C' counter
+    #print(moves)
+    counters = ""
+    for ch in moves:
+        if ch == 'R':
+            counters += 'S'
+        elif ch == 'B':
+            counters += 'K'
+        elif ch == 'L':
+            counters += 'H'
+        else: # Contain 'C'
+            counters += ch
+    
+    print(counters)
+
+
+# if I'm the main program, run my main function,
+#  otherwise just my functions are available
+if(__name__ == '__main__'):
+    main()
--- CS0Fall23/kattis/image_decoding/image_decoding.py
+++ CS0Fall23/kattis/image_decoding/image_decoding.py
@@ -6,39 +6,54 @@ Program Description: https://open.kattis.com/problems/imagedecoding
 def test():
     pass
 
-def main():
+def image_decode(scan_lines):
     pix_sym = ('#','.')
-    scan_lines = int(input())
-    while(scan_lines != 0):
-       pixels = 0
-       image = []
-       error_cond = False
-       last_num_pix = 0
-       for i in range(scan_lines):
-            num_pix_in_line = 0
-            
-            line = input().split() 
-            out_line = ""
-            if(line[0] == '#'):
-                sym = 0
-            else:
-                sym = 1
-            
-            for index in range(1,len(line)):
-                added = pix_sym[sym]
-                added = added*int(line[index])
-                num_pix_in_line += int(line[index])
-                out_line += added
-                sym = (sym+1)%2
-            print(out_line)
-            if((i!=0) and (num_pix_in_line != last_num_pix)):
-                error_cond = True
-            last_num_pix = num_pix_in_line
-       if(error_cond == True):
-           print("Error decoding image")
-       else:
-           print() 
-       scan_lines = int(input())
+    pixels = 0
+    image = []
+    error_cond = False
+    last_num_pix = 0
+    for i in range(len(scan_lines)):
+        num_pix_in_line = 0
+        
+        line = scan_lines[i] 
+        out_line = ""
+        if(line[0] == '#'):
+            sym = 0
+        else:
+            sym = 1
+        
+        for index in range(1,len(line)):
+            added = pix_sym[sym]
+            added = added*int(line[index])
+            num_pix_in_line += int(line[index])
+            out_line += added
+            sym = (sym+1)%2
+        #print(out_line)
+        image.append(out_line)
+        if((i!=0) and (num_pix_in_line != last_num_pix)):
+            error_cond = True
+        last_num_pix = num_pix_in_line
+    if(error_cond == True):
+        #print("Error decoding image")
+        image.append("Error decoding image")
+    else:
+        print()
+
+def main():
+    
+    num_lines = int(input())
+    if(num_lines != 0):
+        scan_lines = []
+        for i in range(num_lines):
+            scan_lines.append(input())
+
+        out_lines = image_decode(scan_lines)
+    else:
+        return
+
+    #while(scan_lines != 0):
+        
+   #    scan_lines = int(input())
 
 # if I'm the main program, run my main function,
 #  otherwise just my functions are available
