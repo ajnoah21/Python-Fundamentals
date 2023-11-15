7f8771c11ef384efa855f37b66698c61616e35d8
Corin Chepko <cchepko2@coloradomesa.edu>
2023-11-15 11:39:13 -0700

    game2.py

--- CS0Fall23/GameSave_fileio/game2.py
+++ CS0Fall23/GameSave_fileio/game2.py
@@ -1,6 +1,23 @@
 import game_open_save as gos
 import pathlib
+import string
 my_path = pathlib.Path(__file__).parent.resolve() #gets the path to file folder
+
+
+def game(name, stats):
+    while(True):
+        guess = input("Guess a letter: ")
+        stats["total_guesses"] += 1
+        if( guess not in string.ascii_letters):
+            print("Try entering a single chacter, a letter preferably: ")
+        else:
+            break
+    if( guess.lower() == 'c'):
+        stats["Wins"] += 1
+    else:
+        stats["Loses"] += 1
+        
+    stats["times_played"] += 1
 
 def main():
     name = input("What your name?")
@@ -12,7 +29,12 @@ def main():
         print(f"{stat} {stats[stat]}") 
         #stat is a dictionary item, so stat is the key, stats[stat] is value
 
-    stats["Loses"] = 2
+    game(name, stats)
+
+    for stat in stats:
+        print(f"{stat} {stats[stat]}")
+
+    input("Press anykey and enter to exit.")
 
     gos.write_file(filename, stats) # saves game
 
