1561bfec6b0cb950137f98715dad0df9f0a150b5
Corin Chepko <cchepko2@coloradomesa.edu>
2023-11-15 11:05:33 -0700

    worksheet2

---
+++ CS0Fall23/worksheet2.py
@@ -0,0 +1,21 @@
+'''
+(#1)
+List 3 container objects (objects that contain multiple values of information)
+we have used in this Python class.
+'''
+
+
+
+#2 Consider the following code:
+def add_two(a, b):
+    a+b+1
+print(add_two(1,3))
+#2 What is the output of the code?
+'''
+a.) 5
+b.) 4
+c.) nothing
+d.) 3
+'''
+#3 What imporvments can be done to the code so it prints the
+#3 correct sum of 1 and 3?
