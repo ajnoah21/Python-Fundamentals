e1efc0a9454c2f29151dd5f472ba76f8276c8596
Corin Chepko <cchepko2@coloradomesa.edu>
2023-11-28 12:37:23 -0700

    Updated example plotting from file

--- CS0Fall23/scipy/2.py
+++ CS0Fall23/scipy/2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+
 x_vals = np.linspace(0, 2*math.pi, 100)
 print(x_vals)
 
@@ -9,7 +10,7 @@ y_vals = np.sin(x_vals)
 
 print(y_vals)
 
-plt.plot(x_vals[0:50],y_vals[0:50], color="green")
-plt.plot(x_vals[0:50],y_vals[0:50]+x_vals[0:50], color="red")
+plt.plot(x_vals,y_vals, color="green")
+plt.plot(x_vals,y_vals+x_vals, color="red")
 plt.title("My first Python Plot!")
 plt.show()
--- CS0Fall23/scipy/3.py
+++ CS0Fall23/scipy/3.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import pathlib
 
-x_vals = np.linspace(0, 10, 10)
-print(x_vals)
+my_path = pathlib.Path(__file__).parent.resolve()
+filename = f"{my_path}/in_data.txt"
 
-with open("in_data.txt") as f_data:
-    lines = f_data.readlines()
+with open(filename) as f_data:
+    lines = f_data.readline().split(sep=',')
     y_vals = list(map(float, lines))
     np.array(y_vals)
+
+x_vals = np.linspace(0, 10, len(y_vals))
+#print(x_vals)
 
 plt.plot(x_vals,y_vals)
 plt.show()
