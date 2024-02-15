7ba95ed7f4155b2a0e0d411e7242145339f3d69d
Corin Chepko <cchepko2@coloradomesa.edu>
2024-02-15 16:34:01 -0700

    sf

--- labs/functions/bijele/bijele.py
+++ labs/functions/bijele/bijele.py
@@ -10,19 +10,27 @@ Algorithm Steps:
         taking the difference between actual pieces and pieces required
 '''
 
-king, queen, rooks, bishops, knights, pawns = input().split()
+def solution(dif1, dif2, dif3, dif4, dif5, dif6):
+    #FIXME put your print statement here
+    pass # erase this once you print the solution
+
+
+king, queen, rooks, bishops, knights, pawns = map(int, input().split())
 
 req_king = 1
 req_queen = 1
 req_rooks = 2
 req_bishops = 2
+
 # FIXMEs 3,4 make varibles for remaining pieces (2 knights, 8 pawns)
 
 dif1 = req_king- king
 dif2 = req_queen-queen
-dif3 = req_bishops-bishops
-dif4 = req_rooks-rooks
+dif3 = req_rooks-rooks
+dif4 = req_bishops-bishops
+
 
 #FIXMEs 5,6: make dif5 and dif6 
 
 #FIXME 7: make a function that prints the solution
+solution(dif1, dif2, dif3, dif4, dif5, dif6)
